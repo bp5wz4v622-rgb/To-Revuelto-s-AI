7ffe89522b05b3ec9ff6b25083deb146690c58a6
bp5wz4v622-rgb <bp5wz4v622@privaterelay.appleid.com>
2025-11-16 09:37:42 -0400

    Actualizar PositionPaperCorrection.tsx

--- PositionPaperCorrection.tsx
+++ PositionPaperCorrection.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useCallback } from 'react';
-import { correctPositionPaper } from '../services/geminiService';
-import Spinner from '../components/Spinner';
+import { correctPositionPaper } from '../geminiService';
+import Spinner from '../Spinner';
 
 interface FormData {
   commission: string;
