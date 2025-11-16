cdf5d310a04004756a1640a6c13af70bedf743c3
bp5wz4v622-rgb <bp5wz4v622@privaterelay.appleid.com>
2025-11-16 09:59:09 -0400

    Actualizar DeepThinking.tsx

--- DeepThinking.tsx
+++ DeepThinking.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react';
-import { solveProblem } from '../geminiService';
+import { solveProblem } from './geminiService.ts';
 import Spinner from '../Spinner';
 
 const DeepThinking: React.FC = () => {
