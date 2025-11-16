8b74bd5a01daff6a15af15107a7430d5588f7f68
bp5wz4v622-rgb <bp5wz4v622@privaterelay.appleid.com>
2025-11-16 10:04:41 -0400

    Actualizar DeepThinking.tsx

--- DeepThinking.tsx
+++ DeepThinking.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useCallback } from 'react';
 import { solveProblem } from './geminiService.ts';
-import Spinner from '../Spinner';
+import Spinner from './Spinner.tsx';
 
 const DeepThinking: React.FC = () => {
   const [problem, setProblem] = useState('');
