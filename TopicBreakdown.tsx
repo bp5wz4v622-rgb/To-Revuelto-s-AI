eaed78260bde7ba10e648c39887c918325d0554a
bp5wz4v622-rgb <bp5wz4v622@privaterelay.appleid.com>
2025-11-16 09:38:52 -0400

    Actualizar TopicBreakdown.tsx

--- TopicBreakdown.tsx
+++ TopicBreakdown.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useCallback } from 'react';
-import { getTopicBreakdown } from '../services/geminiService';
-import Spinner from '../components/Spinner';
+import { getTopicBreakdown } from '../geminiService';
+import Spinner from '../Spinner';
 
 const TopicBreakdown: React.FC = () => {
   const [topic, setTopic] = useState('');
